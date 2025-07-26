8f22e59f15a4d3b22470b083b5b56e55e5fbd781
Ahmed Omeiza <omeizaahmed81@gmail.com>
2025-07-26 15:52:04 +0100

    added event listener
    Added event listener to listen for "enter" button to start conversion

--- index.js
+++ index.js
@@ -11,17 +11,13 @@ const lenghtEl = document.getElementById("length")
 const volumeEl = document.getElementById("volume")
 const massEl = document.getElementById("mass")
 
-buttonEl.addEventListener('click',  () => {
-  const inputValue = Number(inputEl.value)
-  if(inputValue){
-    convert(inputValue, 3.281, "meters", "feet", lenghtEl)
-    convert(inputValue, 0.264, "liters", "gallons", volumeEl)
-    convert(inputValue, 2.204, "Kilos", "pounds", massEl)
-  }else{
-    inputEl.value = ""
+buttonEl.addEventListener('click',  doConversion)
+
+inputEl.addEventListener('keydown', (e) => {
+  if(e.key === "Enter"){
+    doConversion()
   }
 })
-
 
 function convert(input, constant, metric, imperial, element){
   const imperialValue = Math.round(input * constant * 1000) / 1000
@@ -30,4 +26,15 @@ function convert(input, constant, metric, imperial, element){
   const output = `${input} ${metric} = ${imperialValue} ${imperial} | ${input} ${imperial} = ${metricValue} ${metric}`
 
   element.textContent = output
+}
+
+function doConversion() {
+const inputValue = Number(inputEl.value)
+  if(inputValue){
+    convert(inputValue, 3.281, "meters", "feet", lenghtEl)
+    convert(inputValue, 0.264, "liters", "gallons", volumeEl)
+    convert(inputValue, 2.204, "Kilos", "pounds", massEl)
+  }else{
+    inputEl.value = ""
+  }
 }
